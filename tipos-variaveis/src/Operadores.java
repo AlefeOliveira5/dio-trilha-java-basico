29040516e04f315f26bd679923e6bee608fdace5
AlefeOliveira5 <lefeoliveira@gmail.com>
2023-03-09 16:47:17 -0300

    Exercicio SmartTv

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem" + "Java";
+
+        System.out.println(nomeCompleto);
+    }
+    
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Alefe Oliveira";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
     }
 }
